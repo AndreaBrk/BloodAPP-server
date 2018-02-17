7650fe42b6fbcb8b4caa87fbddcad36e125e36f6
Andrea Borek <borekandrea@gmail.com>
2018-02-17 19:43:17 -0300

    after signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { registrations: "registrations" }
 
   scope module: 'api' do
     namespace :v1, defaults: { format: :json, path: '/' } do
@@ -19,7 +20,6 @@ Rails.application.routes.draw do
           get 'change_status'
         end
       end   
-
     end
   end
 end
