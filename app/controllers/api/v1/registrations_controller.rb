db3fc88ab56d89169bf73a34f34485c9349a7210
Andrea Borek <borekandrea@gmail.com>
2018-02-17 19:55:18 -0300

    after signup

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -4,4 +4,8 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     'https://avbapp.herokuapp.com/login' # Or :prefix_to_your_route
   end
+
+  def after_inactive_sign_up_path_for(resource)
+    'https://avbapp.herokuapp.com/login' # Or :prefix_to_your_route
+  end
 end
