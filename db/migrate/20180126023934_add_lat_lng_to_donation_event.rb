206fbeb3f4f947cfbb7548779ce185789b876c98
Andrea Borek <borekandrea@gmail.com>
2018-02-21 09:16:59 -0300

    fixing bug in the creation

--- db/migrate/20180126023934_add_lat_lng_to_donation_event.rb
+++ db/migrate/20180126023934_add_lat_lng_to_donation_event.rb
@@ -1,6 +1,6 @@
 class AddLatLngToDonationEvent < ActiveRecord::Migration[5.1]
   def change
-    add_column :donation_events, :lat, :decimal, precision: 14, scale: 14
-    add_column :donation_events, :lng, :decimal, precision: 14, scale: 14
+    add_column :donation_events, :lat, :decimal, precision: 8, scale: 2
+    add_column :donation_events, :lng, :decimal, precision: 8, scale: 2
   end
 end
