0b66da3a6e8a2ce1060a4b1eefeba871d2cc586c
Andrea Borek <borekandrea@gmail.com>
2018-02-21 00:03:06 -0300

    added validation

--- app/models/donation_event.rb
+++ app/models/donation_event.rb
@@ -1,4 +1,7 @@
 class DonationEvent < ApplicationRecord
   belongs_to :user
   enum status: [:open, :done]
+  validates :name, presence: name
+  validates :size, presence: name
+  validates :blood_type, presence: name
 end
